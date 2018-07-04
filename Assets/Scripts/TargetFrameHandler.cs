bffff26ca6ea082e6835a5db92c959331d70814e
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2018-07-04 18:40:33 +0300

    Add unit frames

---
+++ Assets/Scripts/TargetFrameHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TargetFrameHandler : MonoBehaviour {
+
+    public GameObject HealthBarPrefab;
+
+    GameObject TargetHealthBar;
+
+    void Start() {
+        TargetHealthBar = Instantiate(HealthBarPrefab, transform);
+    }
+
+    void Update() {
+        if (State.currentPlayer.Target == null) {
+            TargetHealthBar.SetActive(false);
+        } else {
+            TargetHealthBar.transform.GetChild(0).GetComponent<HealthBarHandler>().Player = State.currentPlayer.Target;
+            TargetHealthBar.SetActive(true);
+        }
+    }
+}
