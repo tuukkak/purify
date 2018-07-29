8858715751fdcc1148009c1f1da2cc7d4e66ae0b
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2018-07-29 17:05:31 +0300

    Enable casting, movement networking, code cleaning

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,26 +1,44 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class InputHandler : MonoBehaviour {
 
     float MouseSensitivity = 10f;
-    Movement Movement;
-
-    void Start() {
-        Movement = State.currentPlayer.Movement;
-    }
+    sbyte LastInputX;
+    sbyte LastInputZ;
+    Movement Movement = State.CurrentPlayer.Movement;
+    Hero Hero = State.CurrentPlayer.Hero;
+    List<KeyCode> ActionKeys = new List<KeyCode>() { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Q, KeyCode.E };
 
     void Update() {
         UpdateMovement();
         UpdateTarget();
+        UpdateCasts();
 	}
 
     void UpdateMovement() {
-        Movement.InputX = Input.GetAxisRaw("Horizontal");
-        Movement.InputZ = Input.GetAxisRaw("Vertical");
+        Movement.InputX = (sbyte)Input.GetAxisRaw("Horizontal");
+        Movement.InputZ = (sbyte)Input.GetAxisRaw("Vertical");
+
+        if (Movement.InputX != LastInputX || Movement.InputZ != LastInputZ) {
+            Network.UpdateMovement();
+        }
+
+        LastInputX = Movement.InputX;
+        LastInputZ = Movement.InputZ;
 
         if (Input.GetMouseButton(1)) {
-            Movement.Rotation = Movement.Rotation + (Input.GetAxis("Mouse X") * MouseSensitivity);
+            Movement.Rotation = CalculateRotation();
         }
+    }
+
+    float CalculateRotation() {
+        float Rotation = Movement.Rotation + (Input.GetAxis("Mouse X") * MouseSensitivity);
+        Rotation = (float)Math.Round(Rotation, 3);
+        while (Rotation < 0) Rotation += 360f;
+        while (Rotation > 360) Rotation -= 360f;
+        return Rotation;
     }
 
     void UpdateTarget() {
@@ -28,14 +46,32 @@ public class InputHandler : MonoBehaviour {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(ray, out hit)) {
-                if (hit.transform && hit.transform.gameObject.tag == "Player") {
-                    State.currentPlayer.Target = hit.transform.gameObject.GetComponent<PlayerController>().Player;
-                    return;
-                }
+            if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.tag == "Player") {
+                State.CurrentPlayer.Target = hit.transform.gameObject.GetComponent<PlayerController>().Player;
+                return;
             }
 
-            State.currentPlayer.Target = null;
+            State.CurrentPlayer.Target = null;
         }
+    }
+
+    void UpdateCasts() {
+        foreach(KeyCode key in ActionKeys) {
+            if (Input.GetKeyDown(key)) {
+                Spell Spell = State.spells.FindAll(s => s.HeroId == State.CurrentPlayer.Hero.Id).Find(s => s.KeyCode == key.ToString());
+                if (InRange(caster: State.CurrentPlayer, target: State.CurrentPlayer.Target, spell: Spell)) {
+                    State.SpellActions.Add(new SpellAction(
+                        type: SpellAction.ActionType.Finish,
+                        caster: State.CurrentPlayer,
+                        target: State.CurrentPlayer.Target,
+                        spell: Spell
+                    ));
+                }
+            }
+        }
+    }
+
+    bool InRange(Player caster, Player target, Spell spell) {
+        return Vector3.Distance(caster.GameObject.transform.position, target.GameObject.transform.position) < spell.Range;
     }
 }
