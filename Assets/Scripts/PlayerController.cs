cfbe150a818c2f949cc0c4555477f059771d455f
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2018-07-29 16:57:52 +0300

    Stabilize movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,10 @@ public class PlayerController : MonoBehaviour {
     
 	public Player Player;
 
-	void Update() {
-        float x = Player.Movement.InputX * Time.deltaTime * Player.Hero.Speed;
-        float z = Player.Movement.InputZ * Time.deltaTime * Player.Hero.Speed;
-        transform.Translate(Vector3.ClampMagnitude(new Vector3(x, 0, z), Time.deltaTime * Player.Hero.Speed));
+	void FixedUpdate() {
+        float z = Player.Movement.InputZ * Time.fixedDeltaTime * Player.Hero.Speed;
+        float x = Player.Movement.InputX * Time.fixedDeltaTime * Player.Hero.Speed;
+        transform.Translate(Vector3.ClampMagnitude(new Vector3(x, 0, z), Time.fixedDeltaTime * Player.Hero.Speed));
 
         transform.rotation = Quaternion.Euler(0, Player.Movement.Rotation, 0);
 	}
